00f7e149ffb42a2c0d1e8d375bac85784edbf385
lse30 <38194693+lse30@users.noreply.github.com>
2021-06-05 16:31:42 +1200

    Init base Hanjie Solver

---
+++ Hanjie/HanjieSolver.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hanjie
+{
+    public class HanjieSolver
+    {
+        public static int SanityCheck()
+        {
+            return 1;
+        }
+
+        public static char[] LineSolver(int[] blocks, char[] currentLine)
+        {
+            int numberOfBlocks = blocks.Length;
+            int[] offsets = new int[numberOfBlocks];
+            int maxOffset = currentLine.Length - blocks.Sum() - blocks.Length + 1;
+            bool[] nextLine;
+            bool[] confirmedSquares = new bool[currentLine.Length];
+            bool[] confirmedCrosses = new bool[currentLine.Length];
+            for (int x = 0; x < currentLine.Length; x++)
+            {
+                confirmedSquares[x] = true;
+                confirmedCrosses[x] = true;
+            }
+            int i;
+            int mostRecent = 0;
+            bool validLine;
+            bool impossibleLine;
+
+            while (offsets[0] <= maxOffset)
+            {
+                validLine = true;
+                impossibleLine = false;
+                i = 0;
+                nextLine = new bool[currentLine.Length];
+                for (int blockIndex = 0; blockIndex < blocks.Length; blockIndex++)
+                {
+                    i += offsets[blockIndex];
+                    if (i + blocks[blockIndex] <= currentLine.Length)
+                    {
+                        for (int j = i; j < i + blocks[blockIndex]; j++)
+                        {
+                            nextLine[j] = true;
+                        }
+                        i += blocks[blockIndex] + 1;
+                    }
+                    else
+                    {
+                        impossibleLine = true;
+                        break;
+                    }
+                }
+                if (impossibleLine)
+                {
+                    mostRecent--;
+                    offsets[mostRecent]++;
+                    for (int k = mostRecent + 1; k < offsets.Length; k++)
+                    {
+                        offsets[k] = 0;
+                    }
+                }
+                else
+                {
+                    for (int z = 0; z < currentLine.Length; z++)
+                    {
+                        if ((currentLine[z] == 'S' && !nextLine[z]) || (currentLine[z] == 'x' && nextLine[z]))
+                        {
+                            validLine = false;
+                            break;
+                        }
+                    }
+                    if (validLine)
+                    {
+                        for (int squareIndex = 0; squareIndex < nextLine.Length; squareIndex++)
+                        {
+                            confirmedSquares[squareIndex] = confirmedSquares[squareIndex] && nextLine[squareIndex];
+                            confirmedCrosses[squareIndex] = confirmedCrosses[squareIndex] && !nextLine[squareIndex];
+                        }
+
+                    }
+                    offsets[^1]++;
+                    mostRecent = offsets.Length - 1;
+                }
+            }
+            for (i = 0; i < currentLine.Length; i++)
+            {
+                if (currentLine[i] == '_')
+                {
+                    if (confirmedSquares[i])
+                    {
+                        currentLine[i] = 'S';
+                    }
+                    else if (confirmedCrosses[i])
+                    {
+                        currentLine[i] = 'x';
+                    }
+                }
+            }
+            return currentLine;
+        }
+
+        public static char[] GetVerticalLine(List<char[]> map, int index)
+        {
+            int length = map[0].Length;
+            char[] output = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                output[i] = map[i][index];
+            }
+            return output;
+        }
+
+        public static List<char[]> AddVerticalLine(List<char[]> map, char[] line, int index)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                map[i][index] = line[i];
+            }
+            return map;
+        }
+
+        public static List<char[]> InitMap(int length)
+        {
+            var lines = new List<char[]>();
+
+            for (int i = 0; i < length; i++)
+            {
+                char[] toAdd = new char[length];
+                for (int j = 0; j < length; j++)
+                {
+                    toAdd[j] = '_';
+                }
+                lines.Add(toAdd);
+            }
+            return lines;
+        }
+
+        public static List<char[]> CheckMap(List<char[]> map, List<int[]> horizontalBlocks, List<int[]> verticalBlocks)
+        {
+            for (int i = 0; i < horizontalBlocks.Count; i++)
+            {
+                map[i] = LineSolver(horizontalBlocks[i], map[i]);
+            }
+            for (int i = 0; i < verticalBlocks.Count; i++)
+            {
+                char[] verticalLine = LineSolver(verticalBlocks[i], GetVerticalLine(map, i));
+                map = AddVerticalLine(map, verticalLine, i);
+            }
+            return map;
+        }
+
+        public static void PrintMap(List<char[]> map)
+        {
+            foreach (char[] line in map)
+            {
+                Console.WriteLine();
+                char output;
+                foreach (char c in line)
+                {
+                    output = c == 'S' ? 'S' : '_';
+                    Console.Write($"{output} ");
+                }
+            }
+        }
+
+        public static bool CheckComplete(List<char[]> map)
+        {
+            foreach (char[] line in map)
+            {
+                if (line.Contains('_'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static List<int[]> CreateBlocks(string blockString)
+        {
+            var output = new List<int[]>();
+            string[] BlockStringList = blockString.Split(",");
+            string[] rowDetails;
+            int[] rowBlock;
+            for (int i = 0; i < BlockStringList.Length; i++)
+            {
+                rowDetails = BlockStringList[i].Split(" ");
+                rowBlock = new int[rowDetails.Length];
+                for (int j = 0; j < rowDetails.Length; j++)
+                {
+                    rowBlock[j] = int.Parse(rowDetails[j]);
+                }
+                output.Add(rowBlock);
+            }
+            return output;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string horizontalString = "3,1 3,3 1,1 3,1 1";
+            string verticalString = "3,1 1 1,4,2 2,3";
+
+            var horizontalBlocks = CreateBlocks(horizontalString);
+            var verticalBlocks = CreateBlocks(verticalString);
+
+            var lines = InitMap(horizontalBlocks.Count);
+            int attempts = 0;
+            int maxAttempts = 20;
+            while (!CheckComplete(lines) && attempts < maxAttempts)
+            {
+                lines = CheckMap(lines, horizontalBlocks, verticalBlocks);
+                attempts++;
+            }
+
+
+            if (attempts >= maxAttempts)
+            {
+                Console.WriteLine("Unable to Complete the Puzzle.");
+            }
+            else
+            {
+                PrintMap(lines);
+                Console.WriteLine();
+                Console.WriteLine($"Map completed in {attempts} attempts.");
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ HanjieTest/HanjieSolverTests.cs
@@ -0,0 +1,144 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hanjie;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace HanjieTest
+{
+    [TestClass]
+    public class HanjieSolverTests
+    {
+        [TestMethod]
+        public void SanityTest()
+        {
+            int expected = 1;
+            int actual = HanjieSolver.SanityCheck();
+            Assert.AreEqual(expected, actual, "The Sanity check failed.");
+        }
+
+        [TestMethod]
+        public void Solve7in10LineTest()
+        {
+
+            int[] blocks = { 7 };
+            char[] currentLine = new char[10];
+            for (int i = 0; i < currentLine.Length; i++)
+            {
+                currentLine[i] = '_';
+            }
+            char[] expected = new char[10];
+            for (int i = 0; i < expected.Length; i++)
+            {
+                expected[i] = '_';
+            }
+            expected[3] = 'S';
+            expected[4] = 'S';
+            expected[5] = 'S';
+            expected[6] = 'S';
+            char[] actual = HanjieSolver.LineSolver(blocks, currentLine);
+
+
+            CollectionAssert.AreEqual(expected, actual, "Line solver doesn't work.");
+
+        }
+
+        [TestMethod]
+        public void Solve131in10Test()
+        {
+
+            int[] blocks = { 1, 3, 1 };
+            char[] currentLine = new char[10];
+
+            for (int i = 0; i < currentLine.Length; i++)
+            {
+                currentLine[i] = '_';
+            }
+            currentLine[4] = 'S';
+            currentLine[5] = 'S';
+            currentLine[6] = 'S';
+            currentLine[9] = 'S';
+            char[] expected = new char[10];
+            for (int i = 0; i < expected.Length; i++)
+            {
+                expected[i] = '_';
+            }
+            expected[4] = 'S';
+            expected[5] = 'S';
+            expected[6] = 'S';
+            expected[9] = 'S';
+
+            expected[3] = 'x';
+            expected[7] = 'x';
+            expected[8] = 'x';
+            char[] actual = HanjieSolver.LineSolver(blocks, currentLine);
+
+            CollectionAssert.AreEqual(expected, actual, "Line solver doesn't work.");
+
+        }
+
+        [TestMethod]
+        public void SolveExpertPuzzleTest()
+        {
+            var horizontalBlocks = new List<int[]>
+            {
+                new int[] { 7 },
+                new int[] { 2, 3 },
+                new int[] { 3 },
+                new int[] { 1 },
+                new int[] { 2, 2 },
+                new int[] { 3, 3, 1, 5 },
+                new int[] { 1, 2, 2, 4, 2, 1 },
+                new int[] { 2, 4, 4, 1, 1, 1 },
+                new int[] { 4, 2, 1, 1, 2 },
+                new int[] { 5, 3, 7 },
+                new int[] { 6, 4 },
+                new int[] { 11 },
+                new int[] { 12 },
+                new int[] { 4, 4 },
+                new int[] { 3, 4 },
+                new int[] { 4, 5 },
+                new int[] { 3, 6 },
+                new int[] { 3, 7 },
+                new int[] { 3, 6 },
+                new int[] { 2 }
+            };
+
+            var verticalBlocks = new List<int[]>
+            {
+                new int[] { 1, 1, 3, 1 },
+                new int[] { 2, 2, 3, 2 },
+                new int[] { 1, 1, 3, 2 },
+                new int[] { 1, 2, 6, 4 },
+                new int[] { 1, 2, 9 },
+                new int[] { 1, 1, 8 },
+                new int[] { 1, 1, 6, 1 },
+                new int[] { 2, 2, 4, 2 },
+                new int[] { 1, 2, 2, 2, 2 },
+                new int[] { 2, 1, 3, 2, 3 },
+                new int[] { 1, 1, 1, 2, 3 },
+                new int[] { 3, 2, 4, 4 },
+                new int[] { 5, 8 },
+                new int[] { 1, 9 },
+                new int[] { 3, 2, 5 },
+                new int[] { 1, 2, 2 },
+                new int[] { 2, 1 },
+                new int[] { 3, 1 },
+                new int[] { 1, 2 },
+                new int[] { 4 }
+            };
+
+            var lines = HanjieSolver.InitMap(horizontalBlocks.Count);
+            int attempts = 0;
+            int maxAttempts = 20;
+            while (!HanjieSolver.CheckComplete(lines) && attempts < maxAttempts)
+            {
+                lines = HanjieSolver.CheckMap(lines, horizontalBlocks, verticalBlocks);
+                attempts++;
+            }
+
+
+            Assert.IsTrue(attempts < maxAttempts, "Solver solved puzzled unsuccessfully");
+        }
+    }
+}
