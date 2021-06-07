1e0de17aa83101fef73e6565b3e40e9f0ae6f15b
stefanhall <stefan-hall@hotmail.com>
2021-06-07 14:16:27 +1200

    Refactor variable assignment to use var instead of specfic type declaration.
    This is purely a stylistic thing, however it is a pretty common practise.
    See [here](https://intellitect.com/when-to-use-and-not-use-var-in-c/) for some reading on it

--- HanjieTest/HanjieSolverTests.cs
+++ HanjieTest/HanjieSolverTests.cs
@@ -13,14 +13,14 @@ namespace HanjieTest
         public void Solve7in10LineTest()
         {
 
-            int[] blocks = { 7 };
-            char[] currentLine = new char[10];
-            for (int i = 0; i < currentLine.Length; i++)
+            var blocks = new int[]{ 7 };
+            var currentLine = new char[10];
+            for (var i = 0; i < currentLine.Length; i++)
             {
                 currentLine[i] = '_';
             }
-            char[] expected = new char[10];
-            for (int i = 0; i < expected.Length; i++)
+            var expected = new char[10];
+            for (var i = 0; i < expected.Length; i++)
             {
                 expected[i] = '_';
             }
@@ -28,7 +28,7 @@ namespace HanjieTest
             expected[4] = 'S';
             expected[5] = 'S';
             expected[6] = 'S';
-            char[] actual = HanjieSolver.LineSolver(blocks, currentLine);
+            var actual = HanjieSolver.LineSolver(blocks, currentLine);
 
 
             CollectionAssert.AreEqual(expected, actual, "Line solver doesn't work.");
@@ -39,10 +39,10 @@ namespace HanjieTest
         public void Solve131in10Test()
         {
 
-            int[] blocks = { 1, 3, 1 };
-            char[] currentLine = new char[10];
+            var blocks = new[]{ 1, 3, 1 };
+            var currentLine = new char[10];
 
-            for (int i = 0; i < currentLine.Length; i++)
+            for (var i = 0; i < currentLine.Length; i++)
             {
                 currentLine[i] = '_';
             }
@@ -50,8 +50,8 @@ namespace HanjieTest
             currentLine[5] = 'S';
             currentLine[6] = 'S';
             currentLine[9] = 'S';
-            char[] expected = new char[10];
-            for (int i = 0; i < expected.Length; i++)
+            var expected = new char[10];
+            for (var i = 0; i < expected.Length; i++)
             {
                 expected[i] = '_';
             }
@@ -63,7 +63,7 @@ namespace HanjieTest
             expected[3] = 'x';
             expected[7] = 'x';
             expected[8] = 'x';
-            char[] actual = HanjieSolver.LineSolver(blocks, currentLine);
+            var actual = HanjieSolver.LineSolver(blocks, currentLine);
 
             CollectionAssert.AreEqual(expected, actual, "Line solver doesn't work.");
 
@@ -74,55 +74,55 @@ namespace HanjieTest
         {
             var horizontalBlocks = new List<int[]>
             {
-                new int[] { 7 },
-                new int[] { 2, 3 },
-                new int[] { 3 },
-                new int[] { 1 },
-                new int[] { 2, 2 },
-                new int[] { 3, 3, 1, 5 },
-                new int[] { 1, 2, 2, 4, 2, 1 },
-                new int[] { 2, 4, 4, 1, 1, 1 },
-                new int[] { 4, 2, 1, 1, 2 },
-                new int[] { 5, 3, 7 },
-                new int[] { 6, 4 },
-                new int[] { 11 },
-                new int[] { 12 },
-                new int[] { 4, 4 },
-                new int[] { 3, 4 },
-                new int[] { 4, 5 },
-                new int[] { 3, 6 },
-                new int[] { 3, 7 },
-                new int[] { 3, 6 },
-                new int[] { 2 }
+                new [] { 7 },
+                new [] { 2, 3 },
+                new [] { 3 },
+                new [] { 1 },
+                new [] { 2, 2 },
+                new [] { 3, 3, 1, 5 },
+                new [] { 1, 2, 2, 4, 2, 1 },
+                new [] { 2, 4, 4, 1, 1, 1 },
+                new [] { 4, 2, 1, 1, 2 },
+                new [] { 5, 3, 7 },
+                new [] { 6, 4 },
+                new [] { 11 },
+                new [] { 12 },
+                new [] { 4, 4 },
+                new [] { 3, 4 },
+                new [] { 4, 5 },
+                new [] { 3, 6 },
+                new [] { 3, 7 },
+                new [] { 3, 6 },
+                new [] { 2 }
             };
 
             var verticalBlocks = new List<int[]>
             {
-                new int[] { 1, 1, 3, 1 },
-                new int[] { 2, 2, 3, 2 },
-                new int[] { 1, 1, 3, 2 },
-                new int[] { 1, 2, 6, 4 },
-                new int[] { 1, 2, 9 },
-                new int[] { 1, 1, 8 },
-                new int[] { 1, 1, 6, 1 },
-                new int[] { 2, 2, 4, 2 },
-                new int[] { 1, 2, 2, 2, 2 },
-                new int[] { 2, 1, 3, 2, 3 },
-                new int[] { 1, 1, 1, 2, 3 },
-                new int[] { 3, 2, 4, 4 },
-                new int[] { 5, 8 },
-                new int[] { 1, 9 },
-                new int[] { 3, 2, 5 },
-                new int[] { 1, 2, 2 },
-                new int[] { 2, 1 },
-                new int[] { 3, 1 },
-                new int[] { 1, 2 },
-                new int[] { 4 }
+                new [] { 1, 1, 3, 1 },
+                new [] { 2, 2, 3, 2 },
+                new [] { 1, 1, 3, 2 },
+                new [] { 1, 2, 6, 4 },
+                new [] { 1, 2, 9 },
+                new [] { 1, 1, 8 },
+                new [] { 1, 1, 6, 1 },
+                new [] { 2, 2, 4, 2 },
+                new [] { 1, 2, 2, 2, 2 },
+                new [] { 2, 1, 3, 2, 3 },
+                new [] { 1, 1, 1, 2, 3 },
+                new [] { 3, 2, 4, 4 },
+                new [] { 5, 8 },
+                new [] { 1, 9 },
+                new [] { 3, 2, 5 },
+                new [] { 1, 2, 2 },
+                new [] { 2, 1 },
+                new [] { 3, 1 },
+                new [] { 1, 2 },
+                new [] { 4 }
             };
 
             var lines = HanjieSolver.InitMap(horizontalBlocks.Count);
-            int attempts = 0;
-            int maxAttempts = 20;
+            var attempts = 0;
+            var maxAttempts = 20;
             while (!HanjieSolver.CheckComplete(lines) && attempts < maxAttempts)
             {
                 lines = HanjieSolver.CheckMap(lines, horizontalBlocks, verticalBlocks);
